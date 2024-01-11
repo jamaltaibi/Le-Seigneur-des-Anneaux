97e49d3adc7e57faeee0544916fa368266b0989a
Your Name <jamaltaibi@MacBook-Pro-de-Jamal.local>
2024-01-11 14:30:00 +0100

    le sda

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Récupérer les éléments du DOM
-    var openModalBtn = document.getElementById('openModalBtn');
-    var closeModalBtn = document.getElementById('closeModalBtn');
-    var modal = document.getElementById('myModal');
-  
-    // Gérer l'événement clic pour ouvrir le modal
-    openModalBtn.addEventListener('click', function () {
-      modal.style.display = 'block';
-    });
-  
-    // Gérer l'événement clic pour fermer le modal
-    closeModalBtn.addEventListener('click', function () {
+// Fonction pour ouvrir le modal
+function openModal() {
+  document.getElementById('myModal').style.display = 'block';
+}
+
+// Fonction pour fermer le modal
+function closeModal() {
+  document.getElementById('myModal').style.display = 'none';
+}
+
+// Fermer le modal si l'utilisateur clique en dehors du contenu du modal
+window.onclick = function(event) {
+  const modal = document.getElementById('myModal');
+  if (event.target == modal) {
       modal.style.display = 'none';
-    });
-  
-    // Gérer le clic en dehors du modal pour le fermer
-    window.addEventListener('click', function (event) {
-      if (event.target === modal) {
-        modal.style.display = 'none';
-      }
-    });
-  });
-  
+  }
+}
